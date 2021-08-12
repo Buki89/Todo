8c5d4b1034dedf3b04cfa79a11ea82f5c11923d7
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2021-08-12 14:58:16 +0200

    feat: component CategoryItem

---
+++ todolist/src/components/CategoryItem/CategoryItem.tsx
@@ -0,0 +1,85 @@
+import React, { VFC } from "react";
+import styled, { keyframes } from "styled-components";
+import { FaChevronRight } from "react-icons/fa";
+import { useTasksContext } from "../../context/Tasks/tasks-context";
+
+const changeColor = keyframes`
+  from {
+    background-color: #40375920;
+  }
+  to {
+    background-color: #40375950;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => `${theme.colors.primary.main}20`};
+  padding: 1rem;
+  border-radius: 0.75rem;
+  justify-content: space-between;
+  margin: 0.5rem 0;
+  width: calc(100% - 2rem);
+  &:hover {
+    animation: ${changeColor} 0.4s ease-in-out forwards;
+  }
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    padding: 2vh 1rem;
+  }
+`;
+
+const Category = styled.p`
+  padding: 0;
+  margin: 0;
+  font-size: 1rem;
+  color: #000;
+`;
+
+const Tasks = styled(Category)`
+  color: #00000085;
+  font-size: 0.875rem;
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CategoryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1.5rem;
+`;
+
+const Chevron = styled(FaChevronRight)`
+  color: ${({ theme }) => theme.colors.primary.main};
+`;
+
+type CategoryItemProps = {
+  icon: React.ReactNode;
+  category: string;
+};
+
+const CategoryItem: VFC<CategoryItemProps> = ({ category, icon }) => {
+  const { state } = useTasksContext();
+  console.log("state", state.tasks);
+  const tasks =
+    state?.tasks?.filter((task) => task.category === category).length ?? 0;
+
+  return (
+    <Container>
+      <FlexBox>
+        {icon}
+        <CategoryContainer>
+          <Category>{category}</Category>
+          <Tasks>{`${tasks} tasks`}</Tasks>
+        </CategoryContainer>
+      </FlexBox>
+      <Chevron size="1rem" />
+    </Container>
+  );
+};
+
+export default CategoryItem;
--- todolist/src/components/index.ts
+++ todolist/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as ActionBar } from "./ActionBar";
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as CardHeader } from "./CardHeader";
+export { default as CategoryItem } from "./CategoryItem";
