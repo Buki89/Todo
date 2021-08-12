0b1c5b5144fc45456445537602da34890a427eb7
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2021-08-12 14:56:53 +0200

    feat: component CardHeader

---
+++ todolist/src/components/CardHeader/CardHeader.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const CardHeader: FC = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default CardHeader;
--- todolist/src/components/index.ts
+++ todolist/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ActionBar } from "./ActionBar";
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
+export { default as CardHeader } from "./CardHeader";
