a8e822f0d0beadad805d6276a7ad2e37f9248add
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2021-08-12 14:52:07 +0200

    feat: validators file initialization

---
+++ todolist/src/validators.ts
@@ -0,0 +1,27 @@
+export const isEmail = (value: string) => {
+  if (!value) {
+    return undefined;
+  }
+  if (
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g.test(
+      value
+    )
+  ) {
+    return undefined;
+  }
+  return "Invalid Email";
+};
+
+export const isRequired = (value: string) => {
+  if (value) {
+    return undefined;
+  }
+  return "Required";
+};
+
+export const length = (value: string) => {
+  if (value.length > 7) {
+    return undefined;
+  }
+  return "Password is too short";
+};
