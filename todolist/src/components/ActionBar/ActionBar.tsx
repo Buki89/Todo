d07e5961a19a5540c25546ed80ab472b547f760c
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2021-08-12 14:55:14 +0200

    feat: component ActionBar

---
+++ todolist/src/components/ActionBar/ActionBar.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import ButtonBase from "../Button";
+import Info from "../Info";
+
+const Container = styled.div`
+  display: flex;
+  padding: 1rem;
+  justify-content: space-between;
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    padding: 0 1rem 0.5rem;
+  } ;
+`;
+
+const Button = styled(ButtonBase)`
+  background-color: ${({ theme }) => theme.colors.primary.contrastText};
+  color: ${({ theme }) => theme.colors.primary.main};
+`;
+
+type ActionBarProps = {};
+
+const ActionBar: FC<ActionBarProps> = () => {
+  const history = useHistory();
+
+  const handleClick = useCallback(() => history.push("/create"), [history]);
+  return (
+    <Container>
+      <Info />
+      <Button type="button" onClick={handleClick}>
+        Add New
+      </Button>
+    </Container>
+  );
+};
+
+export default ActionBar;
