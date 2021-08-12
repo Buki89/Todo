f8e4a479e1f1f169c96ae049773139c2dc979dc2
Ondřej Bukartyk <ondrej.bukartyk@gmail.com>
2021-08-12 14:51:42 +0200

    feat: theme file initialization

---
+++ todolist/src/theme.tsx
@@ -0,0 +1,27 @@
+import { DefaultTheme } from "styled-components";
+const theme: DefaultTheme = {
+  borderRadius: "4px",
+  colors: {
+    primary: {
+      main: "#403759",
+
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#e3dfec",
+      contrastText: "#ffffff",
+    },
+    error: {
+      main: "#c21313",
+      contrastText: "#ffffff",
+    },
+    border: {
+      main: "#8e81b1",
+    },
+  },
+  media: {
+    mobile: "450px",
+  },
+};
+
+export default theme;
